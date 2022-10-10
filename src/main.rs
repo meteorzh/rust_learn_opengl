7c38798911a1bbac58dcd2d14eeb0300d4d992eb
wenz <wenz@tcfuture.tech>
2022-10-10 18:15:41 +0800

    transform done!

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 extern crate glium;
 extern crate cgmath;
 
-use std::{time, io::Cursor};
+use std::{time::{self, Instant, SystemTime}, io::Cursor};
 
 #[allow(unused_imports)]
 use glium::{glutin::{self, event, window, event_loop}, Surface};
-use glium::{index::PrimitiveType, glutin::event::{KeyboardInput, VirtualKeyCode, ElementState}, draw_parameters};
+use glium::{index::PrimitiveType, glutin::event::{KeyboardInput, VirtualKeyCode, ElementState}, draw_parameters, texture::CompressedSrgbTexture2d, VertexBuffer, Vertex, IndexBuffer, Program, vertex::MultiVerticesSource};
 
 use cgmath::{prelude::*, vec4};
 
@@ -16,65 +16,7 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    // the main loop
-    event_loop.run(move |event, _, control_flow| {
-        // render_triangle(&display);
-        // 帧率设为60FPS，那么1帧16.66666666~毫秒，取16666667纳秒
-        let next_frame_time = time::Instant::now() + time::Duration::from_nanos(16_666_667);
-        *control_flow = event_loop::ControlFlow::WaitUntil(next_frame_time);
-
-        match event {
-            event::Event::WindowEvent { event, .. } => match event {
-                // Break from the main loop when the window is closed.
-                event::WindowEvent::CloseRequested => {
-                    *control_flow = event_loop::ControlFlow::Exit;
-                },
-                // Redraw the triangle when the window is resized.
-                event::WindowEvent::Resized(..) => {
-                    render(&display);
-                    // render_rectangle(&display, false); // line mode
-                    // render_rectangle(&display, true); // line mode
-                },
-                // key input
-                event::WindowEvent::KeyboardInput { input, .. } => {
-                    if let Some(cf) = handle_keyboard_input(input) {
-                        *control_flow = cf;
-                    }
-                },
-                _ => {},
-            },
-            event::Event::NewEvents(cause) => match cause {
-                event::StartCause::ResumeTimeReached { .. } => (),
-                event::StartCause::Init => (),
-                _ => {},
-            },
-            _ => {},
-        }
-    });
-}
-
-fn handle_keyboard_input(input: KeyboardInput) -> Option<event_loop::ControlFlow> {    
-    let virtual_keycode = input.virtual_keycode;
-    if let None = virtual_keycode {
-        return None;
-    }
-
-    let virtual_keycode = virtual_keycode.unwrap();
-    match virtual_keycode {
-        VirtualKeyCode::Escape => {
-            if input.state == ElementState::Released {
-                return Some(event_loop::ControlFlow::Exit);
-            }
-        },
-        _ => {
-            println!("unsupported keyboard input: {}", input.scancode);
-        }
-    }
-
-    None
-}
-
-fn render(display: &glium::Display) {
+    // 初始化顶点缓冲及着色器等资源
     // building the vertex buffer, which contains all the vertices that we will draw
     let vertex_buffer = {
         #[derive(Copy, Clone)]
@@ -86,7 +28,7 @@ fn render(display: &glium::Display) {
 
         implement_vertex!(Vertex, position, color, tex);
 
-        glium::VertexBuffer::new(display,
+        glium::VertexBuffer::new(&display,
             &[
                 Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0], tex: [0.0, 0.0] },
                 Vertex { position: [-0.5, 0.5, 0.0], color: [0.0, 0.0, 1.0], tex: [0.0, 1.0] },
@@ -97,24 +39,21 @@ fn render(display: &glium::Display) {
     };
 
     // building the index buffer
-    let index_buffer = glium::IndexBuffer::new(display, PrimitiveType::TrianglesList,
+    let index_buffer = glium::IndexBuffer::new(&display, PrimitiveType::TrianglesList,
                                                &[0u16, 1, 2, 0, 2, 3]).unwrap();
 
     let image = image::load(Cursor::new(&include_bytes!("container.jpg")), image::ImageFormat::Jpeg).unwrap().to_rgba8();
     let image_dimensions = image.dimensions();
     let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
-    let opengl_texture = glium::texture::CompressedSrgbTexture2d::new(display, image).unwrap();
+    let opengl_texture = glium::texture::CompressedSrgbTexture2d::new(&display, image).unwrap();
 
     let image2 = image::load(Cursor::new(&include_bytes!("awesomeface.png")), image::ImageFormat::Png).unwrap().to_rgba8();
     let image_dimensions2 = image2.dimensions();
     let image2 = glium::texture::RawImage2d::from_raw_rgba_reversed(&image2.into_raw(), image_dimensions2);
-    let opengl_texture2 = glium::texture::CompressedSrgbTexture2d::new(display, image2).unwrap();
-
-    let trans = cgmath::Matrix4::from_angle_z(cgmath::Deg(90_f32));
-    let trans = trans * cgmath::Matrix4::from_scale(0.5_f32);
+    let opengl_texture2 = glium::texture::CompressedSrgbTexture2d::new(&display, image2).unwrap();
 
     // compiling shaders and linking them together
-    let program = program!(display,
+    let program = program!(&display,
 
         330 => {
             vertex: "
@@ -154,12 +93,70 @@ fn render(display: &glium::Display) {
         }
     ).unwrap();
 
-    // Here we draw the black background and triangle to the screen using the previously
-    // initialised resources.
-    //
-    // In this case we use a closure for simplicity, however keep in mind that most serious
-    // applications should probably use a function that takes the resources as an argument.
-    let draw = move || {
+    let mut degree = 0_f32;
+    let mut scale = 1_f32;
+    let mut scale_step = 0.01_f32;
+    // the main loop
+    event_loop.run(move |event, _, control_flow| {
+        // render_triangle(&display);
+
+        // println!("{:#?}", event);
+        let mut render = false;
+        match event {
+            event::Event::WindowEvent { event, .. } => match event {
+                // Break from the main loop when the window is closed.
+                event::WindowEvent::CloseRequested => {
+                    *control_flow = event_loop::ControlFlow::Exit;
+                },
+                // Redraw the triangle when the window is resized.
+                event::WindowEvent::Resized(..) => {
+                    // render(&display, time::Instant::now(), &degree);
+                    // render_rectangle(&display, false); // line mode
+                    // render_rectangle(&display, true); // line mode
+                },
+                // key input
+                event::WindowEvent::KeyboardInput { input, .. } => {
+                    if let Some(cf) = handle_keyboard_input(input) {
+                        *control_flow = cf;
+                    }
+                },
+                _ => {},
+            },
+            event::Event::NewEvents(cause) => match cause {
+                event::StartCause::ResumeTimeReached { .. } => {
+                    // 帧时间限制达到后可以渲染
+                    render = true;
+                },
+                event::StartCause::Init => {
+                    // 初始化时可以渲染
+                    render = true;
+                },
+                _ => {},
+            },
+            _ => {},
+        }
+        if !render {
+            return;
+        }
+
+        // 帧率设为60FPS，那么1帧16.66666666~毫秒，取16666667纳秒
+        let next_frame_time = time::Instant::now() + time::Duration::from_nanos(16_666_667);
+        *control_flow = event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        degree += 0.1;
+        if scale >= 1.5_f32 {
+            // 开始缩小
+            scale_step = -0.01;
+        }
+        if scale <= 0.3 {
+            // 开始放大
+            scale_step = 0.01;
+        }
+        scale += scale_step;
+
+        let trans = cgmath::Matrix4::from_angle_z(cgmath::Deg(degree));
+        let trans = trans * cgmath::Matrix4::from_scale(scale);
+
         let now = time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs_f64();
         let green_value = (now.sin() / 2.0 + 0.5) as f32;
         // let t = glium::uniforms::UniformValue::CompressedSrgbTexture2d(opengl_texture, Some(glium::uniforms::SamplerBehavior::default()));
@@ -183,11 +180,30 @@ fn render(display: &glium::Display) {
         target.clear_color(0.2, 0.3, 0.3, 1.0);
         target.draw(&vertex_buffer, &index_buffer, &program, &uniforms, &Default::default()).unwrap();
         target.finish().unwrap();
-    };
-
-    // Draw the triangle to the screen.
-    draw();
+    });
 }
+
+fn handle_keyboard_input(input: KeyboardInput) -> Option<event_loop::ControlFlow> {    
+    let virtual_keycode = input.virtual_keycode;
+    if let None = virtual_keycode {
+        return None;
+    }
+
+    let virtual_keycode = virtual_keycode.unwrap();
+    match virtual_keycode {
+        VirtualKeyCode::Escape => {
+            if input.state == ElementState::Released {
+                return Some(event_loop::ControlFlow::Exit);
+            }
+        },
+        _ => {
+            println!("unsupported keyboard input: {}", input.scancode);
+        }
+    }
+
+    None
+}
+
 
 fn matrix4_to_raw<S>(matrix: cgmath::Matrix4<S>) -> [[S; 4]; 4] {
     [
